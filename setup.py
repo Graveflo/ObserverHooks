1c1808da32e15ba5c278a4b7c09caae13f6dfd85
ryan <rammcconnell@gmail.com>
2022-08-01 23:33:45 -0400

    deleted the setup.py by mistake

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="observer-hooks",
-    version="0.1a0",
+    version="1.0.0",
     author="Ryan McConnell",
     author_email="",
     description="Easy, minimalist tools for function side-effects and observer pattern with weak references",
@@ -14,5 +14,5 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.0",
+    install_requires=['ordered-set>=4.1.0']
 )
-
